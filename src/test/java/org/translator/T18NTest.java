942c626df4c0fdf2627b9e21b158eb8c87ece2ee
Markus Jakobsson <markus.jakobsson@vikingline.com>
2018-02-28 17:02:38 +0200

    WiP

--- src/test/java/org/translator/T18NTest.java
+++ src/test/java/org/translator/T18NTest.java
@@ -25,21 +25,21 @@ public class T18NTest {
 		assertEquals("Hello World!", L("Hello World!", Locale.getDefault()));
 		assertEquals("Hej Varld!", L("Hello World!", new Locale("sv", "SE")));
 
-        assertEquals("Hello World!", L("org.translator.1"));
-        assertEquals("Hello World!", L("org.translator.1", Locale.getDefault()));
-        assertEquals("Hej Varld!", L("org.translator.1", new Locale("sv", "SE")));
+		assertEquals("Hello World!", L("org.translator.1"));
+		assertEquals("Hello World!", L("org.translator.1", Locale.getDefault()));
+		assertEquals("Hej Varld!", L("org.translator.1", new Locale("sv", "SE")));
 
 		assertEquals("Hello World!", L(this, "Hello World!"));
 		assertEquals("Hello World!", L(this, "Hello World!", Locale.getDefault()));
 		assertEquals("Hej Varld!", L(this, "Hello World!", new Locale("sv", "SE")));
 
-		// assertEquals("The form contains 1 error and 2 warnings.", L("The form contains {0} #{1=error,?=errors} and {1} #{1=warning,?=warnings}.", 1, 2));
-		// assertEquals("The form contains 2 errors and 1 warning.", L("The form contains {0} #{1=error,?=errors} and {1} #{1=warning,?=warnings}.", Locale.getDefault(), 2, 1));
-		// assertEquals("Formularet innehaller 1 fel och 2 varningar.", L("The form contains {0} #{1=error,?=errors} and {1} #{1=warning,?=warnings}.", new Locale("sv", "SE"), 1, 2));
-		//
-		// assertEquals("The form contains 1 error and 2 warnings.", L(this, "The form contains {0} #{1=error,?=errors} and {1} #{1=warning,?=warnings}.", 1, 2));
-		// assertEquals("The form contains 2 errors and 1 warning.", L(this, "The form contains {0} #{1=error,?=errors} and {1} #{1=warning,?=warnings}.", Locale.getDefault(), 2, 1));
-		// assertEquals("Formularet innehaller 1 fel och 2 varningar.", L(this, "The form contains {0} #{1=error,?=errors} and {1} #{1=warning,?=warnings}.", new Locale("sv", "SE"), 1, 2));
+		assertEquals("The form contains 1 error and 2 warnings.", L("The form contains #{errors} and #{warnings}.", 1, 2));
+		assertEquals("The form contains 2 errors and 1 warning.", L("The form contains #{errors} and #{warnings}.", Locale.getDefault(), 2, 1));
+		assertEquals("Formularet innehaller 1 fel och 2 varningar.", L("The form contains #{errors} and #{warnings}.", new Locale("sv", "SE"), 1, 2));
+
+		assertEquals("The form contains 1 error and 2 warnings.", L(this, "The form contains #{errors} and #{warnings}.", 1, 2));
+		assertEquals("The form contains 2 errors and 1 warning.", L(this, "The form contains #{errors} and #{warnings}.", Locale.getDefault(), 2, 1));
+		assertEquals("Formularet innehaller 1 fel och 2 varningar.", L(this, "The form contains #{errors} and #{warnings}.", new Locale("sv", "SE"), 1, 2));
 	}
 
 }
